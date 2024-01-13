f4976d5552b4da3f9e8134fa1e668b732d4c23a8
@vivian4012 <vivian4012@gmail.com>
2024-01-13 13:41:28 +0100

    roughly have a outlook of projec

--- Activity/ProjectsView.swift
+++ Activity/ProjectsView.swift
@@ -10,8 +10,90 @@ import SwiftUI
 struct ProjectsView: View {
     var body: some View {
         NavigationView {
-            Text("Projects")
-                .navigationTitle("Projects")
+            List{
+                NavigationLink(destination: ProjectContentView(title: "Leg")) {
+                    BoxView(title: "Leg")
+                }
+                .padding()
+                                
+                NavigationLink(destination: ProjectContentView(title: "Back")) {
+                   BoxView(title: "Back")
+                }
+                .padding()
+            }
+            .navigationTitle("Projects")
+            .navigationBarItems(
+                trailing:
+                    NavigationLink(destination: NewProjectView()) {
+                        Image(systemName: "plus")
+                    }
+            )
+        }
+    }
+}
+
+struct BoxView: View {
+    var title: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .fill(Color.white)
+            .overlay(
+                Text(title)
+                    .foregroundColor(.black)
+            )
+    }
+}
+
+struct NewProjectView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hi")
+        }
+        .navigationTitle("New Project")
+    }
+
+}
+
+let checkListData = [
+    CheckListItem(id:0,title: "Squats 30 times"),
+CheckListItem(id:1,title: "Lunges"),
+CheckListItem(id:2,title:"Deadlifts"),
+CheckListItem(id:3,title:"Leg Press"),
+CheckListItem(id:4,title:"Leg Extensions")
+]
+
+struct ProjectContentView: View {
+    var title: String
+    var body: some View {
+        NavigationView {
+            List(checkListData){ item in
+                        CheckView(isChecked: item.isChecked, title: item.title)
+                    }
+                    .font(.title)
+        }
+        .navigationTitle(title)
+    }
+}
+
+
+
+struct CheckListItem:Identifiable{
+    var id:Int
+    var isChecked: Bool = false
+    var title: String
+}
+
+struct CheckView: View {
+    @State var isChecked:Bool = false
+    var title:String
+    func toggle(){isChecked = !isChecked}
+    var body: some View {
+        HStack{
+            Button(action: toggle) {
+                Image(systemName: isChecked ? "checkmark.square" : "square")
+            }
+            Text(title)
         }
     }
 }
