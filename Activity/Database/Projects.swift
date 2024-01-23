4f620cc0f6b97da355ed04b0561e476cf026a086
@vivian4012 <vivian4012@gmail.com>
2024-01-23 10:50:24 +0100

    organize the Projects (database ) init function

--- Activity/Database/Projects.swift
+++ Activity/Database/Projects.swift
@@ -16,14 +16,7 @@ class Projects: Identifiable {
     var startDate: Date  // Date type
     var priority: Int
     
-    init(name: String, startDate: Date, priority: Int = 2) {
-        self.name = name
-        self.tasks = []
-        self.startDate = startDate
-        self.priority = priority
-    }
-    
-    init(name: String, tasks: [Task], startDate: Date, priority: Int = 2) {
+    init(name: String, tasks: [Task] = [], startDate: Date, priority: Int = 2) {
         self.name = name
         self.tasks = tasks
         self.startDate = startDate
@@ -41,15 +34,7 @@ class Task: Identifiable {
     var startDate: Date  // Date type
     var priority: Int
     
-    init(name: String, tags: [String] = [], startDate: Date, priority: Int = 2) {
-        self.name = name
-        self.checked = false
-        self.tags = tags
-        self.startDate = startDate
-        self.priority = priority
-    }
-    
-    init(name: String, checked: Bool, tags: [String] = [], startDate: Date, priority: Int = 2) {
+    init(name: String, checked: Bool = false, tags: [String] = [], startDate: Date, priority: Int = 2) {
         self.name = name
         self.checked = checked
         self.tags = tags
