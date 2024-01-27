16b56ee62b8f1bac9aeeb8161656346b1685b66d
Timur Smoev <timur.smoev@rwth-aachen.de>
2024-01-27 21:22:50 +0100

    Add onboarding
    Co-Authored-By: Dan Cojocaru <dan.cojocaru@rwth-aachen.de>

--- Activity/HomeView.swift
+++ Activity/HomeView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct HomeView: View {
+    @Query var settingsList: [Settings]
+    var settings: Settings? { settingsList.first }
+    
     var body: some View {
+        let title = settings?.firstName == nil ? "Welcome" : "Welcome, \(settings!.firstName!)"
+        
         NavigationStack{
+            if settings == nil {
+                Group {
+                    HStack {
+                        Image(systemName: "info.circle.fill")
+                        VStack(alignment: .leading) {
+                            Text("Hint")
+                                .multilineTextAlignment(.leading)
+                                .font(.title2)
+                            Text("Consider going to the Settings tab to customize the application, set your name or enable notifications")
+                        }
+                    }.padding()
+                }
+                .background(Color.accentColor.opacity(0.4))
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .inset(by: 1)
+                        .stroke(Color.accentColor.opacity(0.6), lineWidth: 1)
+                )
+                .padding()
+            }
+            
             VStack {
                 TaskDetailView(date:Date())
-            }.navigationTitle("Welcome")
+            }.navigationTitle(title)
         }
     }
 }
