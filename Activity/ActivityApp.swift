342ada598f9efbc6eb6d9a814c188ba02c6c22c2
asandy520 <yutinghuang.ai09@nycu.edu.tw>
2024-01-29 22:41:15 +0100

    Refactored and commented on HomeView

--- Activity/ActivityApp.swift
+++ Activity/ActivityApp.swift
@@ -12,11 +12,11 @@ import SwiftData
 @main
 struct ActivityApp: App {
     
-    // The main container for the app's data model, including projects, settings, and stopwatch data.
+    /// The main container for the app's data model, including projects, settings, and stopwatch data.
     let container: ModelContainer = ModelContainer.initializeModelContainer()
 
     var body: some Scene {
-        // The main UI scene of the app, hosting the ContentView.
+        /// The main UI scene of the app, hosting the ContentView.
         WindowGroup {
             ContentView()
         }
@@ -25,19 +25,20 @@ struct ActivityApp: App {
 }
 
 extension ModelContainer {
-    // Initializes and returns a ModelContainer instance, safely handling any potential errors.
+    /// Initializes and returns a ModelContainer instance, safely handling any potential errors.
     static func initializeModelContainer() -> ModelContainer {
         let schema = Schema([
             Project.self,
             Settings.self,
             StopwatchData.self
         ])
+        
         let config = ModelConfiguration()
         
         do {
             return try ModelContainer(for: schema, configurations: config)
         } catch {
-            // Proper error handling could be implemented here.
+            /// Proper error handling could be implemented here.
             fatalError("Failed to initialize the model container: \(error)")
         }
     }
